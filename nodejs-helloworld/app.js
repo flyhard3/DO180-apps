fbbab5c16aac75f05d6c934d98ea64c0e16096df
FLYHARD <flyhard3@users.noreply.github.com>
2020-09-08 16:43:15 +0900

    Update app.js
    test from fyre route openshift.

--- nodejs-helloworld/app.js
+++ nodejs-helloworld/app.js
@@ -2,7 +2,7 @@ var express = require('express');
 app = express();
 
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.send('Hello World FLYHARD 2020/09/07 !');
 });
 
 app.listen(8080, function () {
